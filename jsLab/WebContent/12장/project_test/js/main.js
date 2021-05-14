604da99c5662c86ceefd4f19a7f5ec3f5dacf59d
ssm149 <ssm149@naver.com>
2021-05-14 17:50:50 +0900

    jsLab

--- jsLab/WebContent/12장/project_test/js/main.js
+++ jsLab/WebContent/12장/project_test/js/main.js
@@ -1,15 +1,15 @@
 $(function(){
   /*터치 슬라이드 비주얼 영역*/
    window.mySwipe =$('#mySwipe').Swipe({
+   	 startSlide : 0,
      auto: 3000,  //배너가 3초 간격으로 이동합니다.
      continuous: true, // 반복해서 롤링됩니다.
      callback: function(index, element) { //이벤트가 완료되면 실행합니다.
     
         /*클래스 "active"를 포함하는 불릿 버튼을 비활성화 버튼으로 만들고, 
         "active" 클래스를 삭제합니다.*/
-        $(".touch_bullet .active")
-        .attr("src",$(".touch_bullet .active")
-        .attr("src").replace("on.png","off.png"))
+        $(".touch_bullet .active").attr("src",
+        $(".touch_bullet .active").attr("src").replace("on.png","off.png"))
         .removeClass("active"); 
     
         /*인덱스(index)에 해당하는 불릿 버튼을 활성화된 버튼으로 만들고, 
